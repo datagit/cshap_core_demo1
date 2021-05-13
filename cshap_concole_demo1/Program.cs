7c163c9422430f09d3119a0a6da68ead1dcbd4e8
Dat Dao Man <dat.daoman@grevo.net>
2021-05-13 16:52:08 +0700

    first

---
+++ cshap_concole_demo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace cshap_concole_demo1
+{
+    class Product {
+        public string Name { get; set; }
+        public DateTime Expiry { get; set; }
+        public string[] Sizes { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Product product = new Product();
+            product.Name = "Apple";
+            product.Expiry = new DateTime(2008, 12, 28);
+            product.Sizes = new string[] { "Small" };
+
+            string json = JsonConvert.SerializeObject(product);
+            Console.WriteLine(json);
+            // {
+            //   "Name": "Apple",
+            //   "Expiry": "2008-12-28T00:00:00",
+            //   "Sizes": [
+            //     "Small"
+            //   ]
+            // }
+
+        }
+    }
+}
